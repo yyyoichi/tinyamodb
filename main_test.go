d5dd9b7f9003cb56728bd0a3b1caacffa973ee23
yyyoichi <origamiichiro@gmail.com>
2024-10-14 00:57:47 +0000

    doc readme example

--- main_test.go
+++ main_test.go
@@ -12,7 +12,8 @@ import (
 func Example() {
 	// init db
 	var c tinyamodb.Config
-	c.Table.PartitionKey = "pk" // primary-key
+	c.Table.PartitionKey = "gameId" // primary-key
+	c.Table.SortKey = "timestamp"   // sork-key
 
 	db, err := tinyamodb.New("/tmp/tinyamodb", c)
 	if err != nil {
@@ -21,28 +22,53 @@ func Example() {
 	defer db.Close()
 
 	ctx := context.Background()
-	_, err = db.PutItem(ctx, &tinyamodb.PutItemInput{Item: map[string]types.AttributeValue{
-		"pk":   &types.AttributeValueMemberS{Value: "USER#1"},
-		"name": &types.AttributeValueMemberS{Value: "Taro"},
-		"age":  &types.AttributeValueMemberN{Value: "20"},
-	}})
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+	items := []map[string]types.AttributeValue{
+		{
+			"gameId":    &types.AttributeValueMemberS{Value: "GAME#1"},
+			"action":    &types.AttributeValueMemberS{Value: "ATACK"},
+			"player":    &types.AttributeValueMemberN{Value: "1"},
+			"timestamp": &types.AttributeValueMemberS{Value: "2024/10/14-10:00-0000"},
+		},
+		{
+			"gameId":    &types.AttributeValueMemberS{Value: "GAME#1"},
+			"action":    &types.AttributeValueMemberS{Value: "MOVE"},
+			"player":    &types.AttributeValueMemberN{Value: "2"},
+			"timestamp": &types.AttributeValueMemberS{Value: "2024/10/14-10:01-0000"},
+		},
+		{
+			"gameId":    &types.AttributeValueMemberS{Value: "GAME#1"},
+			"action":    &types.AttributeValueMemberS{Value: "ATACK"},
+			"player":    &types.AttributeValueMemberN{Value: "1"},
+			"timestamp": &types.AttributeValueMemberS{Value: "2024/10/14-10:02-0000"},
+		},
+		{
+			"gameId":    &types.AttributeValueMemberS{Value: "GAME#2"},
+			"action":    &types.AttributeValueMemberS{Value: "MOVE"},
+			"player":    &types.AttributeValueMemberN{Value: "5"},
+			"timestamp": &types.AttributeValueMemberS{Value: "2024/10/14-10:00-0000"},
+		},
+	}
+	for _, item := range items {
+		_, err = db.PutItem(ctx, &tinyamodb.PutItemInput{Item: item})
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
 	}
 
 	output, err := db.GetItem(ctx, &tinyamodb.GetItemInput{
 		Key: map[string]types.AttributeValue{
-			"pk": &types.AttributeValueMemberS{Value: "USER#1"},
+			"gameId":    &types.AttributeValueMemberS{Value: "GAME#1"},
+			"timestamp": &types.AttributeValueMemberS{Value: "2024/10/14-10:00-0000"},
 		},
 	})
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
-	name := output.Item["name"].(*types.AttributeValueMemberS)
-	age := output.Item["age"].(*types.AttributeValueMemberN)
-	fmt.Println(name.Value)
-	fmt.Println(age.Value)
+	action := output.Item["action"].(*types.AttributeValueMemberS)
+	player := output.Item["player"].(*types.AttributeValueMemberN)
+	fmt.Println(action.Value)
+	fmt.Println(player.Value)
 	// Output:
-	// Taro
-	// 20
+	// ATACK
+	// 1
 }
